0925ec360098091c82f1ec220d865d8822a9765d
kratos1700 <hunterstarsa@hotmail.com>
2022-05-08 15:33:46 +0200

    dibujando una imagen y 2 textos centrandolos en la pantalla y uso de style  h6 en tipografia de Materialthem

--- app/src/main/java/com/example/aprendiendo/MainActivity.kt
+++ app/src/main/java/com/example/aprendiendo/MainActivity.kt
@@ -3,38 +3,66 @@ package com.example.aprendiendo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.aprendiendo.ui.theme.AprendiendoTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AprendiendoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            PersonalInfo("Kratos")
+
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun PersonalInfo(name: String) {
+        MaterialTheme() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AprendiendoTheme {
-        Greeting("Android")
+            //pone un elemento encima de otro
+            Column(
+                // podemos usar modificadores
+                //añadimos margenes   usamos el espacio total
+                // aliniamos al centro
+                modifier = Modifier.padding(16.dp).fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                // importamos una imagen i le damos un tamaño con el modificador
+                Image(
+                    painter = painterResource(id = R.drawable.perfil),
+                    contentDescription = "imagen perfil",
+                    modifier = Modifier.height(200.dp)
+                )
+                // damos espacio entre la imagen i texto
+                Spacer(modifier = Modifier.height(15.dp))
+                // usando la tipografia del material al la fuente con un h6
+                Text(text = "Mi nombre es $name", style = MaterialTheme.typography.h6)
+                Text(text = "Aprendiendo JetPack.")
+            }
+
+        }
     }
+
+    /**
+     * Nos permite ver la vista interactiva
+     */
+    @Preview
+    @Composable
+    fun PreviewPersonalInfo() {
+
+        PersonalInfo("Kratos1700")
+    }
+
 }
+
